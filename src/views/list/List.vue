40037c8989a44330077224a9bc873ed228863e95
白佑白 <2537140811@qq.com>
2022-03-31 19:04:00 +0800

    初始化项目结构

---
+++ src/views/list/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  components: {},
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/statistics/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "Statistics",
+  components: {},
+};
+</script>
+
+<style scoped>
+</style>
